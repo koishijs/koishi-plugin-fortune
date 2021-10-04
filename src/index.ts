cd6bbe19594d71e32080d29839af0022d03bf4f1
purerosefallen <78877@qq.com>
2021-10-04 18:24:07 +0800

    fix schema export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Config, MyPlugin } from './plugin';
 export { Config } from './plugin';
 
 export const name = 'fortune';
+const plugin = new MyPlugin();
+export const schema = plugin.schema;
 export function apply(ctx: Context, config: Config) {
-  ctx.plugin(new MyPlugin(), config);
+  ctx.plugin(plugin, config);
 }
