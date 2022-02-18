e87e988b72a9ba721e08f0971a8957507fac462b
purerosefallen <78877@qq.com>
2022-02-18 22:24:41 +0800

    make sure it builds over

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Context } from 'koishi';
 import { PluginConfig, MyPlugin } from './plugin';
 export * from './plugin';
 
-export const name = 'fortune';
 const plugin = new MyPlugin();
-export const Config = plugin.schema;
-export function apply(ctx: Context, config: PluginConfig) {
-  ctx.plugin(plugin, config);
+export default class fortune {
+  static Config: any = plugin.schema;
+  constructor(ctx: Context, config: PluginConfig) {
+    ctx.plugin(plugin, config);
+  }
 }
